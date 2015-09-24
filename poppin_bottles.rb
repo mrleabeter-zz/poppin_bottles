afb9427b39bf2769dbba969d4ae15574f8edf367
Christopher Leabeter <christopherleabeter@gmail.com>
2015-09-24 14:41:44 -0400

    Edited previous solution to run properly and look cleaner.

--- poppin_bottles.rb
+++ poppin_bottles.rb
@@ -1,28 +1,28 @@
 def total_bottles(initial_investment)
+  # What we start with
+  full_bottles, dollars = initial_investment.divmod(2)
   total_bottles = 0
-  # Convert initial purchase to bottles - $2/bottle
-  total_bottles += initial_investment / 2
-  empties = total_bottles
-  cap_empties = empties
-  # devide bottles by 2 for bottle recycle program - quotient is added to total_bottles, quotient and remainder are added to new variable 'empties'
-  while empties >= 2 || cap_empties >= 4
-    q_r_a = empties.divmod(2)
-    total_bottles += q_r_a[0]
-    empties = q_r_a[0] + q_r_a[1]
+  empties = 0
+  caps = 0
 
-    empties_and_caps = cap_empties.divmod(4)
-    total_bottles += empties_and_caps[0]
-    cap_empties = q_r_a[0] + q_r_a[1] + empties_and_caps[0] + empties_and_caps[1]
+  while full_bottles > 0
+    # consume full to make empty bottles and caps
+    empties += full_bottles
+    caps += full_bottles
+    total_bottles += full_bottles
+
+    full_bottles, leftover_empties = empties.divmod(2)
+    empties = leftover_empties
+
+    new_bottles, leftover_caps = caps.divmod(4)
+    full_bottles += new_bottles
+    caps = leftover_caps
   end
-  return total_bottles
+  puts "An initial investment of #{initial_investment} will return a total of #{total_bottles} bottles, with #{empties} empty bottles, #{caps} caps, and $#{dollars} left over."
 end
 
 
-# Test with an initial purchase of $20
-puts total_bottles(20)  # => 26
-
-puts total_bottles(10)  # => 13
-
-# puts total_bottles(30)
-
-# puts total_bottles(99)
+# Test with an initial purchase of *
+puts total_bottles(10)  # => 15
+puts total_bottles(20)  # => 35
+puts total_bottles(30)  # => 55
